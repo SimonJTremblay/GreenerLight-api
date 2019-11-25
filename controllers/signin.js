b75752ad61692224eb719feffc7a6114f62579df
SimonJTremblay <stremblay@coach.ca>
2019-11-25 13:33:04 -0500

    modified signin function to add permissions

--- controllers/signin.js
+++ controllers/signin.js
@@ -12,15 +12,23 @@ const handleSignin = (db, bcrypt) => (req, res) => {
             if(isValid){
                 return db.select('*').from('users')
                     .where('email', '=', email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
+                    .then(
+                        (async function(user){
+                           const permission = await getPermission(user[0].id, db);
+                           user[0].permission = permission.permission;
+                            res.json(user[0])
+                        } )
+                    )
                     .catch(err => res.status(400).json('Unable to get user.'))
             } else {
                 res.status(400).json('wrong credentials')
             }
         })
         .catch(err => res.status(400).json('wrong credentials'))
+}
+
+function getPermission(id, db){
+    return db.select('permission').from('permissions').where('user_id','=',id).then(permission => {return permission[0]})
 }
 
 module.exports = {
